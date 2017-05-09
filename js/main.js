36914d7f0b521d00d9537ed68d8731d92d88a9d9
Maks_Hardy <maksyge@gmail.com>
2017-05-09 17:11:19 +0300

    create main section

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
 ;
 (function($) {
     $(function() {
-        $('.hero__big-slider').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            fade: true,
-            asNavFor: '.hero__small-slider'
-        });
+        // $('.hero__big-slider').slick({
+        //     slidesToShow: 1,
+        //     slidesToScroll: 1,
+        //     arrows: false,
+        //     fade: true,
+        //     adaptiveHeight: false,
+        //     asNavFor: '.hero__small-slider'
+        // });
         $('.hero__small-slider').slick({
             slidesToShow: 5,
             slidesToScroll: 1,
             asNavFor: '.hero__big-slider',
-            dots: true,
+            dots: false,
             centerMode: true,
-            focusOnSelect: true
+            focusOnSelect: true,
+            arrows: false,
         });
 
     })
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 370,
+        gutter: 30
+
+    });
+    $("[data-fancybox]").fancybox({
+        // Options will go here
+    });
 })(jQuery);
