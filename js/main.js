d25b490fd22e68f8d5eedddd320ea5efff4292d1
Maks_Hardy <maksyge@gmail.com>
2017-05-10 17:50:48 +0300

    Finished main section

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 ;
 (function($) {
     $(function() {
-        // $('.hero__big-slider').slick({
-        //     slidesToShow: 1,
-        //     slidesToScroll: 1,
-        //     arrows: false,
-        //     fade: true,
-        //     adaptiveHeight: false,
-        //     asNavFor: '.hero__small-slider'
-        // });
+        $('.hero__big-slider-wrap').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            adaptiveHeight: true,
+            asNavFor: '.hero__small-slider'
+        });
         $('.hero__small-slider').slick({
             slidesToShow: 5,
             slidesToScroll: 1,
-            asNavFor: '.hero__big-slider',
+            asNavFor: '.hero__big-slider-wrap',
             dots: false,
             centerMode: true,
             focusOnSelect: true,
             arrows: false,
+            autoplay: true,
+            autoplaySpeed: 2000,
         });
 
     })
@@ -27,6 +29,7 @@
 
     });
     $("[data-fancybox]").fancybox({
-        // Options will go here
+        scrolling: 'yes',
+        speed: 500,
     });
 })(jQuery);
